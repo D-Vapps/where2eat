bf46c77ee385f98f125b55c23163a27b98b8a95c
Flavia Pittet <pittet.flavia@gmail.com>
2018-02-11 02:54:48 +0100

    wip scenes

---
+++ src/scenes/TimeScene.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View, Image } from 'react-native';
+
+import text from '../styles/text.js';
+import colours from '../styles/colors.js';
+import containers from '../styles/containers.js';
+
+export class TimeScene extends React.Component {
+  render() {
+    return (
+        <Text style={text.subTitle}>Fuck u</Text>
+    )
+  }
+}
