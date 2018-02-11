796a5996593159ecf3686bc258c8a7effcbcda52
Mysteriosis <pacurty@gmail.com>
2018-02-12 00:58:09 +0100

    Add styles components to have every UI tools we need. More infos on https://docs.nativebase.io

--- src/scenes/FindScene.js
+++ src/scenes/FindScene.js
@@ -7,6 +7,13 @@ import colors from '../styles/colors.js';
 import containers from '../styles/containers.js';
 
 export default class FindScene extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: true
+        }
+    }
+
     render() {
         const { params } = this.props.navigation.state;
         let date = new Date();
@@ -14,10 +21,22 @@ export default class FindScene extends React.Component {
         const range = params ? params.range : 5;
         
         console.log(time);
+        if (this.state.isLoading) {
+            return (
+                <View style={containers.content}>
+                    <Text style={text.data}></Text>
+                    <ActivityIndicator size="large" style={{paddingTop: 24}} color={colors.secondary} />
+                </View>
+            );
+        }
+
         return (
-            <View style={containers.content}>
-                <Text style={text.data}></Text>
-            </View>
+            <StyleProvider  style={getTheme()}>
+                <Container>
+                    <Content>
+                    </Content>
+                </Container>
+            </StyleProvider>    
         );
     }
 }
