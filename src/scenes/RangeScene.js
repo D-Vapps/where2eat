64cb7272b073881a3296b04552c9f4d23b5e98f2
Flavia Pittet <pittet.flavia@gmail.com>
2018-02-11 14:04:24 +0100

    range and time scenes

--- src/scenes/RangeScene.js
+++ src/scenes/RangeScene.js
@@ -1,5 +1,58 @@
 import React from 'react';
 
+import { Text, View, Image, Button, Slider } from 'react-native';
+
+
+import text from '../styles/text.js';
+import colours from '../styles/colors.js';
+import containers from '../styles/containers.js';
+
+// import {
+//   MKSlider,
+//   MKButton,
+//   MKTouchable,
+// } from 'react-native-material-kit';
+
 export default class RangeScene extends React.Component {
-    render() { }
+    // const { params } = this.props.navigation.state;
+    // console.log("RangeScene");
+    // const time = params ? params.time : null;
+    constructor(props){
+      super(props);
+      let time = this.props.navigation.state.params.time;
+      this.range = 5;
+    }
+
+    getVal(val){
+      this.range = val;
+      console.log(this.range);
+     }
+
+      render() {
+          return(
+            <View style={containers.content}>
+              <Text style={text.questions}>A quelle distance de vous?</Text>
+              <Text style={text.answers}>{this.range} km</Text>
+
+              <Slider style={{ width: 300 }}
+               step={1}
+               minimumValue={5}
+               maximumValue={50}
+               value={this.range}
+               onValueChange={val => this.setState({ range: val })}
+               onSlidingComplete={ val => this.getVal(val)}/>
+
+              <Button
+                  color={colours.secondary}
+                  title="Lancer la recherche"
+                  onPress = {() => this.props.navigation.navigate('Find', {
+                    time: this.time,
+                    range: this.range
+                  })}
+              />
+
+
+            </View>
+          )
+      }
 }
--- src/scenes/TimeScene.js
+++ src/scenes/TimeScene.js
@@ -1,15 +1,55 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, TouchableOpacity, Button } from 'react-native';
 
 import text from '../styles/text.js';
 import colours from '../styles/colors.js';
 import containers from '../styles/containers.js';
 
+import DateTimePicker from 'react-native-modal-datetime-picker';
+
 export default class TimeScene extends React.Component {
+
+  constructor(props){
+    super(props);
+    let time;
+  }
+
+  state = {
+  isDateTimePickerVisible: false,
+};
+
+_showDateTimePicker = () => this.setState({ isDateTimePickerVisible: true });
+
+_hideDateTimePicker = () => this.setState({ isDateTimePickerVisible: false });
+
+_handleDatePicked = (date) => {
+  console.log('A date has been picked: ', date);
+  this.time = date;
+  this._hideDateTimePicker();
+};
+
   render() {
     return (
       <View style={containers.content}>
-        <Text style={text.subTitle}>Fuck u</Text>
+      <Text style={text.questions}>A quelle heure souhaitez-vous manger?</Text>
+      <View style={{ flex: 1 }}>
+          <TouchableOpacity onPress={this._showDateTimePicker}>
+            <Text style={text.questions}>Choisir</Text>
+          </TouchableOpacity>
+          <DateTimePicker
+            isVisible={this.state.isDateTimePickerVisible}
+            mode='time'
+            onConfirm={this._handleDatePicked}
+            onCancel={this._hideDateTimePicker}
+          />
+        </View>
+        <Button
+          onPress={() => this.props.navigation.navigate('Range', {
+            time: this.time
+          })}
+          title="Suivant"
+          color={colours.secondary}
+        />
       </View>
     )
   }
